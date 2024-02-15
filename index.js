8924239afe0536f823b64c9f57f8a440af7f5e09
Enzo Seschi <enzosseschi@hotmail.com>
2024-02-15 16:44:16 +0000

    Atualizando blógica do js

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-
 const Http = new XMLHttpRequest();
 const url = "https://economia.awesomeapi.com.br/json/last/BTC-BRL";
 Http.open("GET", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
-    console.log(Http.responseText);
+    if (Http.readyState === 4 && Http.status === 200) {
+        try {
+            let response = JSON.parse(Http.responseText);
+            let valor_btc = response.BTCBRL.low;
+            let input_cripto = document.getElementById("input-criptocoin");
+
+            input_cripto.value = valor_btc;
+        } catch (error) {
+            console.error("Erro: ", error);
+        }
+    }
 }
 
+function onInputValue() {
+    let input_brl = document.getElementById("input-money_irl");
+    let input_cripto = document.getElementById("input-criptocoin");
+
+    let response = JSON.parse(Http.responseText);
+    let valor_btc = response.BTCBRL.low;
+
+    parseInt(valor_btc)
+    
+    if (input_cripto.value != "") {
+        let calc = valor_btc * parseInt(input_brl.value)
+        input_cripto.value = calc
+    } else {
+        console.log('teste')
+        input_cripto.value = ""
+    }
+
+}
 
 function CriptoCurrencyLink() {
     let select_cripto = document.getElementById("select-moedas_cripto");
