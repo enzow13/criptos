ee4e44f533141639da707203ec7646f8f688a994
Enzo Seschi <enzosseschi@hotmail.com>
2024-02-16 14:57:28 +0000

    Mudança de cripto tag em XMLHttpRequest

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const Http = new XMLHttpRequest();
-const url = "https://economia.awesomeapi.com.br/json/last/BTC-BRL";
-Http.open("GET", url);
+
+function getCriptoURL() {
+    cripto_name = document.getElementById("select-moedas_cripto").value
+    return `https://economia.awesomeapi.com.br/json/last/${cripto_name}-BRL`
+}
+Http.open("GET", getCriptoURL());
 Http.send();
 
-Http.onreadystatechange = (e) => {
+Http.onreadystatechange = () => {
     if (Http.readyState === 4 && Http.status === 200) {
         try {
-            let response = JSON.parse(Http.responseText);
-            let valor_btc = response.BTCBRL.low;
             let input_cripto = document.getElementById("input-criptocoin");
-
-            input_cripto.value = valor_btc;
+            input_cripto.value = "";
         } catch (error) {
-            console.error("Erro: ", error);
+            console.error(`Erro: ${error} HTTP_STATE: ${Http.readyState}`);
         }
     }
 }
@@ -20,15 +21,15 @@ Http.onreadystatechange = (e) => {
 function onInputValue() {
     let input_brl = document.getElementById("input-money_irl");
     let input_cripto = document.getElementById("input-criptocoin");
-
     let response = JSON.parse(Http.responseText);
     let valor_btc = response.BTCBRL.low;
 
     parseInt(valor_btc)
+    parseInt(input_brl.value)
     
-    if (input_cripto.value != "") {
-        let calc = valor_btc * parseInt(input_brl.value)
-        input_cripto.value = calc
+    if (input_brl.value != "") {
+        let calc = input_brl.value / valor_btc
+        input_cripto.value = calc.toFixed(6)
     } else {
         console.log('teste')
         input_cripto.value = ""
