f96a2d2182ae456d5902af2c0dd258df7abed519
Enzo Seschi <enzosseschi@hotmail.com>
2024-02-19 13:48:02 +0000

    Corrigindo erros e atualizando métodos para pegar os valores ("GET")

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const Http = new XMLHttpRequest();
 
-function getCriptoURL() {
-    cripto_name = document.getElementById("select-moedas_cripto").value
-    return `https://economia.awesomeapi.com.br/json/last/${cripto_name}-BRL`
+addEventListener("DOMContentLoaded", () => {
+    let cripto_name = document.getElementById("select-moedas_cripto").value;
+    
+    if (cripto_name != "BTC") {
+        Http.open("GET", `https://economia.awesomeapi.com.br/json/last/${cripto_name}-BRL`);
+        Http.send();
+    } else {
+        Http.open("GET", `https://economia.awesomeapi.com.br/json/last/BTC-BRL`);
+        Http.send();
+    }
+})
+
+function onChangeCripto() {
+    let cripto_name = document.getElementById("select-moedas_cripto").value;
+    Http.open("GET", `https://economia.awesomeapi.com.br/json/last/${cripto_name}-BRL`);
+    Http.send();
 }
-Http.open("GET", getCriptoURL());
-Http.send();
 
 Http.onreadystatechange = () => {
     if (Http.readyState === 4 && Http.status === 200) {
@@ -19,24 +30,28 @@ Http.onreadystatechange = () => {
 }
 
 function onInputValue() {
-    let input_brl = document.getElementById("input-money_irl");
+    let cripto_name = document.getElementById("select-moedas_cripto").value;
+    let input_brl = parseFloat(document.getElementById("input-money_irl").value);
     let input_cripto = document.getElementById("input-criptocoin");
     let response = JSON.parse(Http.responseText);
-    let valor_btc = response.BTCBRL.low;
+    let valor_cripto = parseFloat(response[cripto_name + "BRL"].low)
 
-    parseInt(valor_btc)
-    parseInt(input_brl.value)
-    
-    if (input_brl.value != "") {
-        let calc = input_brl.value / valor_btc
-        input_cripto.value = calc.toFixed(6)
+    /* Printa o valor das variáveis para ver erros
+
+    console.log(`Valor: ${valor_cripto}, Tipo: ${typeof valor_cripto}/n Valor: ${input_brl}, Tipo: ${typeof input_brl}`)
+    console.log(`${valor_cripto + input_brl}`)
+    */
+
+    if (!isNaN(input_brl)) {
+        let calc = input_brl / valor_cripto;
+        input_cripto.value = calc.toFixed(6);
     } else {
-        console.log('teste')
         input_cripto.value = ""
     }
 
 }
 
+// Levar ao site de gráficos (coin market)
 function CriptoCurrencyLink() {
     let select_cripto = document.getElementById("select-moedas_cripto");
     let url = `https://coinmarketcap.com/pt-br/currencies/${select_cripto.value}/`;
